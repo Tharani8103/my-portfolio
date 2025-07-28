0a88a9dd066358b278e026e2b6ee67939bc619e9
Tharani8103 <tharani.mca2024@adhiyamaan.in>
2025-07-28 21:52:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { HashRouter as Router } from 'react-router-dom';
+
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from './components/Education'; 
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+
 export default function App() {
     return (
-        <>
-            <Navbar />
-            <Hero />
-            <About />
-            <Skills />
-            <Projects />
-            <Education />
-            <Contact />
-            <Footer />
-        </>
+        <Router>
+            <div>
+                <Navbar />
+                <Hero />
+                <About />
+                <Skills />
+                <Projects />
+                <Education />
+                <Contact />
+                <Footer />
+            </div>
+        </Router>
     );
 }
